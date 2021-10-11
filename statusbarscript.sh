c759ed068dd9355cc431313a220e2b18e6465be9
EinTim <57799248+EinTim23@users.noreply.github.com>
2021-10-11 11:37:29 +0200

    Update statusbarscript.sh

--- statusbarscript.sh
+++ statusbarscript.sh
@@ -7,8 +7,8 @@ brightness=$(cat /sys/class/backlight/radeon_bl0/brightness)
 speicher=$(df -h | grep '/dev/mapper/vg-root' | awk -F ' ' '{print $4}')
 date=$(date -d '-2 hours' '+%d.%m.%Y %X')
 sound=$(pamixer --get-volume)
-cpu=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage "%"}')
-res="CPU: "$cpu" Memory: "$ram"MB SSD: "$speicher" Sound: "$sound"% Brightness: "$brightness"% Date: "$date" Battery: "$bat"%"
+cpu=$(top -b -n1 | grep -i "Cpu(s)" | awk '{print $2 + $4}')
+res="\uf2db : "$cpu" \uf1c0 : "$ram"MB \uf0a0 : "$speicher" \uf028: "$sound"% \uf185 : "$brightness"% \uf017 : "$date" \uf241 : "$bat"%"
 xsetroot -name "$res"
 sleep 1
 done
